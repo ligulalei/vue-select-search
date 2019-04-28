0affeb538917009aad3ecf32f27dcb914f18a2a5
ligulalei <2364636038@qq.com>
2019-04-28 15:04:05 +0800

    ListSelect

---
+++ src/components/lib/ListSelect.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        1
+    </div>
+</template>
+<script>
+export default {
+    data () {
+
+    },
+    watch: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+}
+</script>
+<style>
+</style>
