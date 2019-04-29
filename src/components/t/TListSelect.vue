e4a63587b6634ef554b090d65f4d4865d0735f51
ligulalei <2364636038@qq.com>
2019-04-29 14:51:38 +0800

    优化

--- src/components/t/TListSelect.vue
+++ src/components/t/TListSelect.vue
@@ -1,14 +1,21 @@
 <template>
-<div>
+<div class='selectdropdown'>
     <input 
     :value='searchValue'
     @input="searchValue = $event.target.value"
+    @focus="openOptions"
     />
+    <div class="showOptionList"
+         :style='optionStyle'
+    >
     <template v-for="(option,index) in showOptions">
-        <div :key="index">
+        <div class="option" 
+              @click.stop="selectOption(option)"
+              :key="index">
             {{option.value}}-{{option.text}}
         </div>
     </template>
+    </div>
  </div>
 </template>
 <script>
@@ -17,7 +24,8 @@ export default {
   mixins: [commonMixin],
   data () {
     return {
-      searchValue: ''
+      searchValue: '',
+      showOption: false
     }
   },
   watch: {
@@ -27,25 +35,61 @@ export default {
     showOptions () {
       if (this.searchValue) {
         return this.options.filter((option) => {
-          console.log('*')
-          console.log(option.text)
           return this.filterPredicate(option.value, this.searchValue)
         })
       } else {
-        console.log(this.searchValue)
         return this.options
+      }
+    },
+    optionStyle () {
+      return {
+        display: this.showOption ? 'block' : 'none'
       }
     }
   },
   methods: {
-
+    openOptions () {
+      this.showOption = true
+    },
+    hideOptions () {
+      this.showOption = false
+    },
+    selectOption (option) {
+      console.log('-----------')
+      this.searchValue = option.value
+      this.hideOptions()
+    }
   },
   created () {
     console.log('--------t------------')
-    console.log(this.options)
-    console.log(this.hi)
   }
 }
 </script>
-<style>
+
+<style scoped>
+.selectdropdown{
+  position: relative;
+  width:300px;
+  margin:0 auto;
+}
+.selectdropdown input{
+  width:100%;
+  padding:0;
+  outline: none;
+  height:30px;
+}
+.showOptionList{
+  max-height:200px;
+  height:auto;
+  width:300px;
+  overflow-y: auto;
+  text-align:left;
+}
+.showOptionList .option{
+  cursor: pointer;
+  padding:10px;
+}
+.showOptionList .option:hover{
+  background: rgba(0,0,0,0.1);
+}
 </style>
